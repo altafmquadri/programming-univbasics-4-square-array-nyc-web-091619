7c2b5549458fb879ace9cade744e53f839d30c91
Altaf M Quadri <altafmquadri@gmail.com>
2019-08-29 01:55:17 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  counter = 0
+  square_array = []
+  while array[counter] < array.length do
+    square_array.push((array[counter]*array[counter]))
+    counter += 1
+  end
+  square_array
 end
